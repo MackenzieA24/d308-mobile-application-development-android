5cba2b9fc9bf1a595ab9def747d0f79c937b6652
aylor <maylor5@wgu.edu>
2024-05-14 16:09:36 -0500

    Added user login

--- app/src/main/java/com/example/d308app/UI/MainActivity.java
+++ app/src/main/java/com/example/d308app/UI/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.d308app.UI;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,26 +19,41 @@ import com.example.d308app.R;
 public class MainActivity extends AppCompatActivity {
 
     public static int numAlert;
+    EditText usernameInput;
+    EditText passwordInput;
+    Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Button button=findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener(){
 
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this, Vacations.class);
-                intent.putExtra("test", "Information sent");
-                startActivity(intent);
-            }
-        });
+        usernameInput = findViewById(R.id.username_input);
+        passwordInput = findViewById(R.id.password_input);
+        loginBtn = findViewById(R.id.login_btn);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameInput.getText().toString();
+                String password = passwordInput.getText().toString();
+                if (username.isEmpty() || password.isEmpty()) {
+                    Toast.makeText(MainActivity.this, "Please provide valid username and password", Toast.LENGTH_LONG).show();
+                    Log.i("Login Failed", "Empty username or password");
+                } else {
+                    Log.i("Login Success", "Username: " + username + " and Password: " + password);
+                    Intent intent = new Intent(MainActivity.this, Vacations.class);
+                    intent.putExtra("test", "Information sent");
+                    startActivity(intent);
+                }
+            }
         });
     }
 }
